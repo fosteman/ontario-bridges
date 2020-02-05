2fb2d95c0eba6b76756e75427139f273fffcf679
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-02-05 12:38:01 -0500

    Updates

---
+++ precache-manifest.3d4422d71c2c4db36952649f4a266271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947fb52687c5a07377fa0bfd14032a17",
+    "url": "/ontario-bridges/index.html"
+  },
+  {
+    "revision": "bca86762388ea0f09b79",
+    "url": "/ontario-bridges/static/css/2.36f280e5.chunk.css"
+  },
+  {
+    "revision": "6ca9350b649bbb0bd8e1",
+    "url": "/ontario-bridges/static/css/main.e7f80c5f.chunk.css"
+  },
+  {
+    "revision": "bca86762388ea0f09b79",
+    "url": "/ontario-bridges/static/js/2.eb63d6cc.chunk.js"
+  },
+  {
+    "revision": "6ca9350b649bbb0bd8e1",
+    "url": "/ontario-bridges/static/js/main.b41353c4.chunk.js"
+  },
+  {
+    "revision": "6930def4dc0d3085c335",
+    "url": "/ontario-bridges/static/js/runtime~main.683ecc0f.js"
+  }
+]);
